c676154d61f883a90b53bfb4eb69a27678d295f0
prachikhanna <er.prachikhanna@gmail.com>
2020-12-16 23:03:35 -0600

    post added

--- client/upload.js
+++ client/upload.js
@@ -1,3 +1,5 @@
+  var uploadedImages = [] ;
+
   function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
 
@@ -15,8 +17,9 @@
       reader.onload = (function(theFile) {
         return function(e) {
           // Render thumbnail.
+          uploadedImages[i] = e.target.result;
           var span = document.createElement('span');
-          span.innerHTML = ['<img class="col-25" src="', e.target.result,
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
                             '" title="', escape(theFile.name), '"/>'].join('');
           document.getElementById('list').insertBefore(span, null);
         };
@@ -32,20 +35,15 @@
 function handleIDSelect(evt){
 
     var idFiles = evt.target.files[0];
-    console.log("evt.target", evt.target.files);
-    console.log("idFiles.type", idFiles.type , typeof(idFiles.type))
-
       // Only process image files.
     if (idFiles.type.includes('image/')) {
-        console.log("true");
         var reader = new FileReader();
             reader.readAsDataURL(idFiles);
-
             reader.onload = function(e) {
                 // result contains loaded file.
-                console.log(reader.result);
+                e.target.result[2] = reader.result;
               var span = document.createElement('span');
-              span.innerHTML = ['<img class="col-25" src="', e.target.result,
+              span.innerHTML = ['<img class="thumb" src="', e.target.result,
                                 '" title="', escape(idFiles.name), '"/>'].join('');
               document.getElementById('list').insertBefore(span, null);
             }
@@ -53,9 +51,15 @@ function handleIDSelect(evt){
     else{
         console.log("error");
     }
+}
 
 
+  function handleFileUpload(evt) {
+
+    console.log(uploadedImages[1]);
 
 }
+
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
   document.getElementById('idFiles').addEventListener('change',handleIDSelect,false);
+  document.getElementById('submitButton').addEventListener('click',handleFileUpload,false);
